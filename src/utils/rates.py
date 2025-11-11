7636876fa40c67df8f208913fe75e6b73b8a1a6c
martyna <codingbee77@tlen.pl>
2025-11-11 11:15:54 +0100

    Use internal API to get all currency rates

--- src/utils/rates.py
+++ src/utils/rates.py
@@ -1,6 +1,6 @@
 import json
 import os
-from typing import Final
+from typing import Final, List
 
 import requests
 from dotenv import load_dotenv
@@ -8,20 +8,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BASE_URL: Final[str] = os.environ.get("BASE_URL")
+API_URL: Final[str] = os.environ.get("API_URL")
 
 
-def get_rates(mock: bool = False) -> dict:
-    rates_path = os.path.join(os.path.dirname(__file__), "rates.json")
+def get_rates(mock: bool = False) -> List[dict]:
+    rates_path = os.path.join(os.path.dirname(__file__), "rates_DB.json")
     if mock:
         with open(rates_path, "r") as file:
             rates_response = json.load(file)
             return rates_response.get("rates")
 
-    # TODO: Remove it and use rates from database
-    # Fetch API_KEY dynamically at runtime
-    api_key = os.environ.get("API_KEY")
-    payload: dict = {"access_key": api_key}
-    request = requests.get(url=BASE_URL, params=payload)
-    data: dict = request.json()
-
-    return data
+    # TODO: Change to actual external API call in the future
+    rates_response = requests.get(f"{API_URL}/currencies/").json()
+    return rates_response
