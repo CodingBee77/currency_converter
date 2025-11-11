cc6aef2fa66d3169c8cb5d4ef95b5baaff5edd6c
martyna <codingbee77@tlen.pl>
2025-11-11 11:17:15 +0100

    Adjust currency_converter.py file to use data from internal API, add new mock json file that matches with DB structure

--- run_script.py
+++ run_script.py
@@ -1,13 +1,14 @@
+from typing import List
+
 from src.currency_converter import convert_currency
 from src.utils.rates import get_rates
 
 
 def main():
-    data: dict = get_rates(mock=True)
-    rates: dict = data.get("rates")
+    rates = get_rates(mock=False)
 
-    convert_currency(base_currency="EUR", target_currency="SEK", amount=100, rates=rates)
-    convert_currency(base_currency="USD", target_currency="GBP", amount=50, rates=rates)
+    convert_currency(base_currency="EUR", target_currency="SEK", amount=104, rates=rates)
+    convert_currency(base_currency="GBP", target_currency="GBP", amount=50, rates=rates)
 
 
 if __name__ == "__main__":
--- src/currency_converter.py
+++ src/currency_converter.py
@@ -1,15 +1,20 @@
-def get_currency(currency: str, rates: dict) -> float:
+from typing import List
+
+
+def get_currency(currency: str, rates: List[dict]) -> float:
     currency = currency.upper()
-    if currency in rates.keys():
-        return rates.get(currency)
+    currency_rate = next((rate for rate in rates if rate["code"] == currency), None)
+
+    if currency_rate:
+        return currency_rate["rate"]
     else:
         raise ValueError(f"'{currency}' is not a valid currency.")
 
 
-def convert_currency(base_currency: str, target_currency: str, amount: float, rates: dict) -> float:
+def convert_currency(base_currency: str, target_currency: str, amount: float, rates: List[dict]) -> float:
     base_rate: float = get_currency(base_currency, rates)
-    vs_rate: float = get_currency(target_currency, rates)
+    target_rate: float = get_currency(target_currency, rates)
 
-    conversion: float = round((vs_rate / base_rate) * amount, 2)
+    conversion: float = round((target_rate / base_rate) * amount, 2)
     print(f"{amount:,.2f} ({base_currency}) is: {conversion:,.2f} ({target_currency})")
     return conversion
