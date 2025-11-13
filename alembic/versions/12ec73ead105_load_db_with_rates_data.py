cddb329d7f0f38b66548c6d8189c6edc85090ace
martyna <codingbee77@tlen.pl>
2025-11-13 10:08:33 +0100

    Add alembic script to load DB with mock data

---
+++ alembic/versions/12ec73ead105_load_db_with_rates_data.py
@@ -0,0 +1,40 @@
+"""Load DB with rates data
+
+Revision ID: 12ec73ead105
+Revises: d4ca94b2c2b6
+Create Date: 2025-11-10 16:19:38.951978
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql import table, column
+import json
+
+# revision identifiers, used by Alembic.
+revision = '12ec73ead105'
+down_revision = 'd4ca94b2c2b6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # Load rates from rates.json
+    with open('src/utils/rates.json', 'r') as file:
+        rates = json.load(file).get('rates', {})
+
+    # Insert rates into currencies table
+    currencies_table = table('currencies',
+                             column('code', sa.String),
+                             column('name', sa.String),
+                             column('rate', sa.Float),
+                             )
+
+    op.bulk_insert(currencies_table,
+                   [
+                       {'code': code, 'name': 'TBD', 'rate': rate} for code, rate in rates.items()
+                   ]
+                   )
+
+
+def downgrade():
+    op.execute("DELETE FROM currencies")
