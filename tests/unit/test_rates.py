04eea0afa8aa9c358d101ab079fd4c0bc909da3a
martyna <codingbee77@tlen.pl>
2025-11-17 18:30:55 +0100

    Update test_rates.py

--- tests/unit/test_rates.py
+++ tests/unit/test_rates.py
@@ -1,27 +1,34 @@
+import pytest
+import requests.exceptions
+
 from src.utils.rates import get_rates
+import random
 
 
 def test_get_rates_mock_true_valid():
     rates = get_rates(mock=True)
-    assert rates.get("success") is True
-    assert rates.get("timestamp") is not None
-    assert rates.get("base") is not None
-    assert rates.get("date") is not None
-    assert rates.get("rates") is not None
+    index = random.randrange(0, len(rates))
+    assert isinstance(rates, list)
+    assert isinstance(rates[index].get("code"), str)
+    assert isinstance(rates[index].get("name"), str)
+    assert isinstance(rates[index].get("rate"), float)
+    assert len(rates[index]) == 3
 
 
-def test_get_rates_valid_api_key():
+@pytest.mark.skipif(
+    True,
+    reason="Skipping test to avoid external API call during testing. "
+    "Uncomment this decorator to run the test when the external API is available.")
+def test_get_rates_mock_is_false():
     rates = get_rates(mock=False)
-    assert rates.get("success") is True
-    assert rates.get("timestamp") is not None
-    assert rates.get("base") is not None
-    assert rates.get("date") is not None
-    assert rates.get("rates") is not None
+    index = random.randrange(0, len(rates))
+    assert isinstance(rates, list)
+    assert isinstance(rates[index].get("code"), str)
+    assert isinstance(rates[index].get("name"), str)
+    assert isinstance(rates[index].get("rate"), float)
+    assert len(rates[0]) == 3
 
 
-def test_get_rates_invalid_api_key(monkeypatch):
-    monkeypatch.setenv("API_KEY", "INVALID_API_KEY")
-    rates = get_rates(mock=False)
-    print(rates)
-    assert "error" in rates
-    assert "You have not supplied a valid API Access Key" in rates["error"]["message"]
+def test_get_rates_mock_is_false_no_response():
+    with pytest.raises(requests.exceptions.ConnectionError):
+        rates = get_rates(mock=False)
