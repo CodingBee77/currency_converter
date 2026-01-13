f7ee5eea91ab19c26d73efeb252ba72ae58a2463
martyna <codingbee77@tlen.pl>
2026-01-13 11:36:48 +0100

    Fix formatting and use full response comparison for test_get_currencies

--- tests/integration/test_currencies.py
+++ tests/integration/test_currencies.py
@@ -13,6 +13,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
 load_dotenv()
 
+
 def test_get_currencies(currencies, client):
     response = client.get("/currencies")
     all_currencies = currencies.query(models.Currency).all()
@@ -20,13 +21,13 @@ def test_get_currencies(currencies, client):
 
     assert response.status_code == 200
     assert len(response.json()) == 5
-    # assert response.json() == all_currencies_json
+    assert response.json() == all_currencies_json
+
 
 def test_get_currencies_error(currencies, client):
     # Simulate a database error by dropping the currencies table
-    currencies.execute(text('DROP TABLE IF EXISTS currencies'))
+    currencies.execute(text("DROP TABLE IF EXISTS currencies"))
     with pytest.raises(ProgrammingError):
         response = client.get("/currencies")
         print(response.status_code)
         assert response.status_code == 500
-
